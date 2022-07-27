18703812fa7a64eab2a58754b6e0e9aab0e13ff1
Ian Luna Quiroz <ianmlunaq@gmail.com>
2022-07-26 20:28:04 -0700

    Learning

---
+++ workingWithNumbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()  
+{
+    double num = 5.0 + 4.5;
+    printf("%f\n", num);
+
+    printf("%f\n", pow(2, 3));
+
+    printf("%f\n", sqrt(36));
+
+    printf("%f\n", ceil(36.8547));  // Rounds up
+
+    printf("%f\n", floor(36.8547));  // Rounds down
+    
+    return 0;
+}
