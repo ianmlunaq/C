b14557b65f4522a31dd3a6c10506924464f1729b
Ian Luna Quiroz <ianmlunaq@gmail.com>
2022-07-28 18:32:47 -0700

    Commit

---
+++ user-input.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()  
+{
+    int age;
+    printf("Enter your age: ");
+    scanf("%d", &age);
+    
+    return 0;
+}
--- vars.c
+++ vars.c
@@ -7,6 +7,12 @@ int main()
     double gpa = 3.7;
     char grade = 'A';
     char phrase[] = "Strings are made by creating an array of characters";
+    printf("%d %f %c %s\n", age, gpa, grade, phrase);
+
+    const int IMPORTANT_NUM = 42;
+    printf("%d\n", IMPORTANT_NUM);
+    //IMPORTANT_NUM = 7;    //This will throw an error if uncommented
+    printf("%d\n", IMPORTANT_NUM);
     
     return 0;
 }
